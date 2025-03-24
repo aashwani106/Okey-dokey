caf436d39edfe80c4460115d43db9a373134e05a
Ashwani Mishra <ashwani01superman@gmail.com>
2025-03-25 02:53:16 +0530

    minor changes

--- src/LoginPage.jsx
+++ src/LoginPage.jsx
@@ -1,43 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
+import { Eye, EyeOff } from "lucide-react";
 import loginSidebar from "./assets/login-sidebar.png";
 import logo from "./assets/okey-dokey-logo.png";
 import google from "./assets/google.png";
+import { Mail,Lock } from "lucide-react";
+
 function LoginPage() {
+  const [showPassword, setShowPassword] = useState(false);
+
   return (
     <div className="flex h-screen bg-white">
       {/* Sidebar */}
       <div
-        className="hidden md:w-1/2 bg-cover bg-center md:block text-white p-8 flex flex-col justify-center items-center h-full"
+        className="md:w-1/2 bg-cover bg-center md:block text-white p-8 flex flex-col justify-center items-center h-full"
         style={{ backgroundImage: `url(${loginSidebar})` }}
       >
         <div className="flex flex-col justify-center items-center">
-          <h1 className="text-4xl font-bold text-center !important md:mr-50 md:mt-100">
+          <h1 className="text-4xl font-bold text-center md:mr-50 md:mt-100">
             Welcome to Okey Dokey
           </h1>
-          <p className="text-lg text-center !important md:mr-50">
+          <p className="text-lg text-center md:mr-50">
             Find your People, Your Passion, Your Purpose
           </p>
         </div>
       </div>
 
       {/* Login Form */}
-
-      <div className="w-full md:w-1/2 flex h-full items-center justify-center flex-col" >
+      <div className="w-full md:w-1/2 flex h-full items-center justify-center flex-col">
         <div className="logo mb-3 flex justify-center">
           <img src={logo} className="w-[100px]" alt="Logo" />
         </div>
-        <div  className="border-2 border-gray-300 rounded-lg shadow-lg p-6 bg-white w-full sm:w-3/4 md:w-100">
+        <div className="second-box border-2 border-gray-300 rounded-lg shadow-xl p-6 bg-white w-full sm:w-3/4 md:w-100">
           <h2 className="text-2xl font-semibold text-center">Welcome Back</h2>
           <p className="text-gray-500 mb-4 text-center">
             Please login to continue
           </p>
 
-          <button className="bg-white border border-gray-300 rounded-full py-2 px-4 mb-4 w-full flex items-center justify-center">
-            <img
-              src={google}
-              alt="Google Logo"
-              className="w-6 h-6 mr-2"
-            />
+          <button className="bg-white border border-gray-300 rounded-full py-2 px-4 mb-4 w-full flex items-center justify-center text-gray-500">
+            <img src={google} alt="Google Logo" className="w-6 h-6 mr-2" />
             Continue with Google
           </button>
 
@@ -47,7 +47,7 @@ function LoginPage() {
             <div className="border-t border-gray-300 flex-grow"></div>
           </div>
 
-          <form className="w-full">
+          <form className="">
             <div className="mb-4">
               <label
                 className="block text-gray-700 text-md font-bold mb-2"
@@ -55,45 +55,62 @@ function LoginPage() {
               >
                 Email address
               </label>
-              <input
-                className="border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
-                id="username"
-                type="text"
-                placeholder="example@gmail.com"
-              />
+              <div className="relative">
+                <input
+                  className="border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 pl-10"
+                  id="username"
+                  type="text"
+                  placeholder="example@gmail.com"
+                />
+                <div className="absolute inset-y-0 left-3 flex items-center pointer-events-none text-gray-500">
+                  <Mail size={20} />
+                </div>
+              </div>
             </div>
 
-            <div className="mb-6">
+            <div className="mb-6 relative">
               <label
-                className="block text-gray-700 text-sm font-bold mb-2"
+                className="block text-gray-700 text-sm font-bold mb-2 "
                 htmlFor="password"
               >
                 Password
               </label>
-              <input
-                className="border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
-                id="password"
-                type="password"
-                placeholder="Password"
-              />
+              <div className="relative">
+                <input
+                  className="border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 pl-10 pr-10"
+                  id="password"
+                  type={showPassword ? "text" : "password"}
+                  placeholder="Password"
+                />
+                <div className="absolute inset-y-0 left-3 flex items-center pointer-events-none text-gray-500">
+                  <Lock size={20} />
+                </div>
+                <button
+                  type="button"
+                  className="absolute inset-y-0 right-3 flex items-center justify-center text-gray-600"
+                  onClick={() => setShowPassword(!showPassword)}
+                >
+                  {showPassword ? <EyeOff size={20} /> : <Eye size={20} />}
+                </button>
+              </div>
             </div>
 
             <div className="flex justify-end">
               <a
-                className="text-indigo-500 hover:text-indigo-800 text-sm font-bold"
+                className="text-black hover:text-indigo-800 text-sm font-bold underline"
                 href="#"
               >
                 Forgot password?
               </a>
             </div>
 
-            <button className="bg-indigo-500 hover:bg-indigo-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline w-full mt-3">
+            <button className="bg-[#8d34dc] hover:bg-indigo-700 text-white font-bold py-2 px-4 rounded-full focus:outline-none focus:shadow-outline w-full mt-3">
               Login
             </button>
 
             <div className="mt-5">
               <p className="text-center">
-                New user?{" "}
+                <span className="text-gray-500"> New user? </span>
                 <a href="#" className="text-blue-500">
                   Create account
                 </a>
